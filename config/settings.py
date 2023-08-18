2a4679d7b5cfe367c590e8b2e52b29748e650d71
kar <kareum712@gmail.com>
2023-08-18 12:29:53 +0900

    HighFive final

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=gq*e4y+lao+j&1jz=u(un4ox-a7lo+hd@g5)64re44$^80md%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
