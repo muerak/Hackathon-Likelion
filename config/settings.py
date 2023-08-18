d435275d8bf2d403a1be70a3dcf7394f8262b43a
kar <kareum712@gmail.com>
2023-08-18 17:55:09 +0900

    allowed host update

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure-=gq*e4y+lao+j&1jz=u(un4ox-a7lo+hd@g5)64re44$^80md%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com"
+]
 
 
 # Application definition
